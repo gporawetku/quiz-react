b91984efa160184af32cb8a11ef2d1147287d08c
Porawet Ku <gporawet.ku@gmail.com>
2024-03-02 18:28:15 +0700

    chore: add bg and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
 import { ReactNode, useState } from "react";
 
-const Card = ({ children }: { children?: ReactNode }) => {
+const Card = ({
+  header,
+  children,
+  footer,
+}: {
+  header?: ReactNode;
+  children?: ReactNode;
+  footer?: ReactNode;
+}) => {
   return (
-    <div className="min-h-80 max-w-5xl rounded bg-white p-5 shadow-lg">
-      {children}
+    <div className="h-[50vh] w-[50vw] rounded-2xl bg-white p-5 shadow-lg">
+      <div className="flex flex-col">
+        <div className="p-5 text-3xl font-bold">{header}</div>
+        <div className="p-5">{children}</div>
+        <div className="p-5">{footer}</div>
+      </div>
     </div>
   );
 };
 
 const AnswerCard = ({ onOpen }: any) => {
+  const header = "Answer";
+
   return (
     <>
-      <Card>
-        <div className="" onClick={onOpen}>
+      <Card header={header}>
+        <div
+          className="flex h-full w-full items-center justify-center"
+          onClick={onOpen}
+        >
           close
         </div>
       </Card>
@@ -21,30 +38,28 @@ const AnswerCard = ({ onOpen }: any) => {
 };
 
 const QuestionCard = ({ onOpen }: any) => {
+  const header = "Quiz";
   return (
     <>
-      <Card>
-        <div className="flex flex-col" onClick={onOpen}>
-          <div className="p-5">Quiz System Analysis</div>
-          <div className="flex flex-col justify-center gap-4 p-5 text-3xl font-extralight">
-            <p>
-              1. Object oriented systems analysis and design is more feasible
-              than ever today due to:
-            </p>
-            <ul className="flex flex-col gap-2 pl-6">
-              <li>
-                a. The increased speed of processors and storage (and decrease
-                in cost - aka "Moore's Law")
-              </li>
-              <li>b. Textbooks on object oriented analysis and design</li>
-              <li>
-                c. The recent discovery by Bill Gates of the object oriented
-                paradigm
-              </li>
-              <li>d. Graphical User Interfaces</li>
-              <li>e. Neo's use of object orientated SA&D in the Matrix</li>
-            </ul>
-          </div>
+      <Card header={header}>
+        <div className="text-2xl font-extralight" onClick={onOpen}>
+          <p>
+            1. Object oriented systems analysis and design is more feasible than
+            ever today due to:
+          </p>
+          <ul className="flex flex-col gap-2 pl-6">
+            <li>
+              a. The increased speed of processors and storage (and decrease in
+              cost - aka "Moore's Law")
+            </li>
+            <li>b. Textbooks on object oriented analysis and design</li>
+            <li>
+              c. The recent discovery by Bill Gates of the object oriented
+              paradigm
+            </li>
+            <li>d. Graphical User Interfaces</li>
+            <li>e. Neo's use of object orientated SA&D in the Matrix</li>
+          </ul>
         </div>
       </Card>
     </>
@@ -54,7 +69,7 @@ const QuestionCard = ({ onOpen }: any) => {
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="flex h-screen items-center justify-center bg-slate-100">
+    <div className="flex h-screen items-center justify-center bg-emerald-900">
       {!isOpen ? (
         <QuestionCard onOpen={() => setIsOpen(!isOpen)} />
       ) : (
